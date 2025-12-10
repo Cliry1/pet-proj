d2a135c85ac1f48497834091046e237412b0e70b
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2025-12-10 18:43:26 +0100

    s

--- src/services/auth.js
+++ src/services/auth.js
@@ -60,7 +60,6 @@ export const loginUser = async (payload) => {
   const isEqual = await bcrypt.compare(payload.password, user.password);
   if (!isEqual) throw createHttpError(401, 'Unauthorized');
 
-  await SessionCollection.deleteOne({ userId: user._id });
 
   const newSession = createSession();
 
